e7cb184cfd5f1ac8b44dbba20c47b8a03f2e9546
caiocroccia22 <caiocroccia22@gmail.com>
2024-03-23 09:13:13 -0300

    Inserindo os inputs das variaveis

--- gera_recibo.py
+++ gera_recibo.py
@@ -1,11 +1,22 @@
 from fpdf import FPDF
 
-#Layout
+# Variaveis
+
+cliente = input("Digite o nome do cliente\n")
+valor = input("Digite o valor: \n")
+# assinatura = input("Digite sua assinatura: \n")
+# nome = input("Digite o seu nome:\n")
+importância_de = input("Digite o campo a importância de: \n")
+#Layout do recibo
 
 pdf = FPDF()
-
-pdf.add_page()
+# orientation= 'L' -> gera o arquivo em paisagem
+pdf.add_page(orientation='L')
 pdf.set_font("Arial", "", 20)
-pdf.image("img/recibo.png", x=0, y=0)
+pdf.image("img/recibo.png", x=-10, y=0)
+pdf.text(260, 30, valor)
+pdf.text(70, 50, cliente)
+pdf.text(70, 63, importância_de)
+# pdf.text(10, 50, nome)
 pdf.output("recibo.pdf")
 print("Recibo gerado com sucesso!")
